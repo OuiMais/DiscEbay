3e613224e38095779b030c9a6a0c38a99d0ffd00
OuiMais <flohofbauer@icloud.com>
2024-05-12 12:19:41 +0200

    First

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+    Projet : DiscEbay
+    Date Creation : 12/05/2024
+    Date Revision : 12/05/2024
+    Entreprise : 3SC4P3
+    Auteur: Florian HOFBAUER
+    Contact :
+    But : Bot discord de type leboncoin ou ebay
+"""
+
